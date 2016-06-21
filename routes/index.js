394b0da7cfee5901187ce2440670078443b4e168
Roberto Ortega <berto.ort@gmail.com>
2016-06-21 09:43:27 -0600

    routes set

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,28 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.post('/login', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.post('/signup', function(req, res, next) {
+  res.redirect('/');
+});
+
+router.get('/logout', function(req, res, next) {
+  res.redirect('/');
+});
+
 module.exports = router;
